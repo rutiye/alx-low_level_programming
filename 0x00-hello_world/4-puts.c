14e58ad795e96454159cb4c9e1c0a43772bd495b
rutiye <ruthmoges57@gmail.com>
2022-07-01 08:15:07 -0700

    my next commit for low level programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
-#include <studio.h>
+#include <studio.h>
 
-int main(void)
+int main(void)
 {
-puts("\"programming is like building a multilingual puzzle");
+puts("\"programming is like building a multilingual puzzle");
 return (0);
 }
