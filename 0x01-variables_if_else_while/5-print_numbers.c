eefa780b3417debb8931d447cdc4df96dc80d476
rutiye <ruthmoges57@gmail.com>
2022-07-04 15:03:07 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char num[10] = "0123456789";
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(num[i]);
+	}
+	putchar('\n');
+	return (0);
+}
