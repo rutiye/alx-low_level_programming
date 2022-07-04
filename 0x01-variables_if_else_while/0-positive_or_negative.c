9cd259a25a1bde072196a5ba76b010065fc6e14d
rutiye <ruthmoges57@gmail.com>
2022-07-04 12:36:09 -0700

    first assignment for variables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,25 +1,21 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
 /**
- *main -> assign a random number to the variable n each time it is executed.and prints out
- *based on a condition
- *Return : always 0
- */
-int main(void)
-{
-int n;
+ *main -> assign a random number to the variable n each time it is executed and prints out
+ *based on a condition
+ *Return : always 0 
+ */
+int main(void) 
+{ 
+int n;
 
 srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n > 0)
-	printf("%d is positive\n", n);
-if (n == 0)
-	printf("%d is zero\n", n);
-if (n < 0)
-{
-	printf("%d is negative\n", n);
-}
-return (0);
-}
+n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{	
+	printf("%d is positive\n", n);
+	}else if (n == 0)
+	printf("%d is zero\n", n); 
+	if (n < 0)
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,30 +3,32 @@
 #include <stdio.h>
 
 /**
-*main -> assign a random number to the variable n each time it is executed
-* print the last digit of the number stored in the variable n
-*Return : always 0
-*/
+ * main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-int n;
+	int n, la;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-printf("Last digit of %d is",n);
-if (n >5)
-{
-	printf("greater than 5");
-}
-if ( n == 0)
-{
-	printf("0");
-}
-if (n < 6 && n !=0)
-{
-	printf("less than 0 not 0");
-}
-printf("\n);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-return (0);
+	la = n % 10;
+
+	if (la > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, la);
+	}
+	else if (la == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, la);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, la);
+	}
+
+	return (0);
 }
