9b1e66203c8ffde358ebd9ed9ed03b4baa117a4e
rutiye <106909112+rutiye@users.noreply.github.com>
2022-07-05 03:58:09 +0300

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
- *main - Program that prints all possible different combinations of two digits
+ * main - Program that prints all possible different combinations of two digits
  *
  * Return: Always 0 (Success)
+ *
  */
 
 int main(void)
