5f4d21aaae1625546064a039c3c6fb91641acb29
rutiye <ruthmoges57@gmail.com>
2022-07-05 12:26:15 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	char str[8] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
