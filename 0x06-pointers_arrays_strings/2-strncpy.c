bef3c886f56cbc5086ff556d062a6340dea38374
rutiye <ruthmoges57@gmail.com>
2022-07-13 12:37:23 -0700

    string work

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,15 +1,14 @@
 #include "main.h"
 #include <string.h>
-
 /**
- * _strncat -> function to append some charx
- * @dest: first param
- * @src: second param
- * @n: 3rd param
+ * _strncpy -> for copying purpose
+ * @dest: param1
+ * @src: param2
+ * @n: param3
  * Return: string
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
-	strncat(dest, src, n);
+	strncpy(dest, src, n);
 	return (dest);
 }
